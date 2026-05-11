d6e524ae034dc7d6bd4285e92deda81d92da7975
ramesh-kunwar <rameshkunwar1110@gmail.com>
2026-05-11 20:12:08 +0545

    create poll dto

--- backend/src/module/polls/dto/create-poll.dto.ts
+++ backend/src/module/polls/dto/create-poll.dto.ts
@@ -1,20 +1,26 @@
 import * as z from "zod";
 
-export const pollSchema = z.object({
+export const createPollSchema = z.object({
   title: z.string().min(1, "title is required").max(255),
   isAnonymous: z.boolean().default(false),
+  expiresAt: z.string().datetime().optional(),
   questions: z
     .array(
       z.object({
         question_text: z.string().min(1, "question is required").max(255),
+        isMandatory: z.boolean().default(false),
+        order: z.number().int().min(0),
         options: z
-          .array(z.string().min(1, "option is required").max(255))
+          .array(
+            z.object({
+              option_text: z.string().min(1, "option is required").max(255),
+              order: z.number().int().min(0),
+            })
+          )
           .min(2, "at least 2 options are required"),
       })
     )
     .min(1, "at least 1 question is required"),
-  order: z.number().default(0),
-  expiresAt: z.date().optional(),
 });
 
-export type PollDto = z.infer<typeof pollSchema>;
+export type CreatePollDto = z.infer<typeof createPollSchema>;
