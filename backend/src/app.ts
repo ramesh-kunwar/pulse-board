3469fcc934963b77d2e70dcff079ae94a0942f50
ramesh-kunwar <rameshkunwar1110@gmail.com>
2026-05-15 14:20:13 +0545

    feat: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
 import express, { Express } from "express";
+import cors from "cors";
 import authRouter from "./module/auth/auth.routes.js";
 import { errorHandler } from "./common/middleware/error.middleware.js";
 import cookieParser from "cookie-parser";
 import pollsRouter from "./module/polls/polls.routes.js";
 const app: Express = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true, // ← required for cookies to work cross-origin
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
